135481e0f50e32bcc109c547e13ac4e69026b6a8
Yuma Satake <satous.y@icloud.com>
2023-11-04 12:21:35 +0900

    Refactor database functions and fix SQL query

--- dbfunc.php
+++ dbfunc.php
@@ -1,6 +1,6 @@
 <?php
-function dbConnect()
-{
+
+function dbConnect() {
     $host = 'localhost';
     $dbname = 'seminar2db';
     $username = 'root';
@@ -9,25 +9,22 @@ function dbConnect()
         $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         return $pdo;
     } catch (PDOException $e) {
-        echo "エラーメッセージ：" . $e->getMessage();
+        echo "エラーメッセージ: " . $e->getMessage();
     }
 }
 
-function selectProducts()
-{
+function selectProducts() {
     $pdo = dbConnect();
     try {
-        $stmt = $pdo->prepare("SELECT * FROM PRODUCTS ORDER BY CODE");
+        $stmt = $pdo->prepare("SELECT * FROM PRODUCTS ORDER BY PCODE");
         $stmt->execute();
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
-
     $products = [];
     $i = 0;
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         $products[$i] = [
-            'pcode' => $row['pcode'],
             'pname' => $row['pname'],
             'pprice' => $row['pprice'],
             'pfilename' => $row['pfilename']
@@ -37,9 +34,18 @@ function selectProducts()
     return $products;
 }
 
-function insOrder($data)
-{
+function insOrder($data) {
     $pdo = dbConnect();
-    $stmt = $pdo->prepare('');
+    $stmt = $pdo->prepare("SELECT COALESCE(MAX(ORDNO), 0) + 1 AS newordno FROM orders");
+    $stmt->execute();
+    $newordno = $stmt->fetch(PDO::FETCH_ASSOC)['newordno'];
+    $stmt = $pdo->prepare("INSERT INTO ORDERS (ORDNO, ORDDATE, PCODE, QUANTITY, ODELIDATE, OACTELIDATE, CNAME, CMAIL) VALUES (:ordno, SYSDATE(), :pcode, :oquantity, :odelidate, NULL, :cname, :cmail)");
+    // バインド変数に代入
+    $stmt->bindValue(':ordno', $newordno, PDO::PARAM_INT);
+    $stmt->bindValue(':pcode', $data['pcode'], PDO::PARAM_STR);
+    $stmt->bindValue(':oquantity', $data['oquantity'], PDO::PARAM_INT);
+    $stmt->bindValue(':odelidate', $data['odelidate'], PDO::PARAM_STR);
+    $stmt->bindValue(':cname', $data['cname'], PDO::PARAM_STR);
+    $stmt->bindValue(':cmail', $data['cmail'], PDO::PARAM_STR);
     $stmt->execute();
 }
